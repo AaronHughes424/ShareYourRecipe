7c89c2d5ed7c1a5e9eeac69a7c57dfa31d855fb9
AaronHughes424 <aaronhughes424@gmail.com>
2022-02-16 12:46:02 +0000

    added form for sign up

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -14,6 +15,12 @@ app.secret_key = os.environ.get("SECRET_KEY")
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    return render_template("sign-up.html")
+
 
 
 if __name__ == "__main__":
