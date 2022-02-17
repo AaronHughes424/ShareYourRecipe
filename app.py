efefe6be1841c53df3a7403d223c972b32ef10b2
AaronHughes424 <aaronhughes424@gmail.com>
2022-02-17 20:55:22 +0000

    fixed indetation issues

--- app.py
+++ app.py
@@ -70,10 +70,10 @@ def login():
 
         if existing_user:
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
-                    return redirect(url_for(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
                         "profile", username=session["user"]))
             else:
                 flash("Incorrect Username and/or Password")
